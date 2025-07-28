6e42bcf9cf40916119e2ea9e7552e293f87eb8d5
manjeyy <mrmanjeylala00@gmail.com>
2025-07-29 01:00:44 +0545

    add stuffs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@
 
 import Hero from "../../components/section/Hero";
 import NavBar from "../../components/section/NavBar";
-import { MotionEffect } from "../../components/ui/AnimatedContainer";
-import MarqueeText from "../../components/ui/MarqueeText";
-import { motion } from 'framer-motion'
 
 export default function Home() {
+
   return (
     <div>
       <NavBar />
       <Hero />
       <Missions />
-      <Events/>
+      <Events />
     </div>
   );
 }
@@ -28,11 +26,21 @@ function Missions() {
 }
 
 function Events() {
+  const events = [
+    { id: 1, name: "SUMMER ENRICHMENT" },
+    { id: 2, name: "IIC QUEST" },
+    { id: 3, name: "CREATIVE CLASH" },
+  ];
+
+  if (events.length === 0) return null;
+
   return (
-    <div className="w-screen h-screen bg-slate-800">
-        <div>
-          
+    <div className="w-screen h-auto bg-black flex items-start justify-center pb-12">
+      <div className="max-w-[1240px] w-full h-auto mt-0 flex flex-col md:px-0 p-4">
+        <h2 className=" font-bold text-[8vw] md:text-[5vw]  text-white text-left my-6 md:my-12">EVENTS</h2>
+        <div className="grid grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-6 h-full w-full p-0">
         </div>
+      </div>
     </div>
-  )
+  );
 }
